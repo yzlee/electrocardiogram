560b53ed1574c9e445c80110f4621c4634ec123c
liyongzhi <121007469@qq.com>
2016-09-03 14:17:58 +0800

    version 1.2.5  bug fix

--- ecgview/src/main/java/cc/liyongzhi/ecgview/ECGSubView.java
+++ ecgview/src/main/java/cc/liyongzhi/ecgview/ECGSubView.java
@@ -124,7 +124,7 @@ public class ECGSubView {
                 scaling *= 1.05;
             }
 
-            if (thumbnailMode && maxValue < subHeight / 12 && maxValueReal > subHeight / 2) {
+            if (thumbnailMode && maxValue < subHeight / 12 && maxValueReal > subHeight / 8) {
                 scaling = 0.5f;
             }
 
